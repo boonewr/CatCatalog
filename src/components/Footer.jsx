f405c23b21a9cc05a03b7762b899a486472f4233
Robbie Boone <wrboone@ncsu.edu>
2025-08-06 18:35:54 -0400

    Removed images

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+    // Remember to change class to className
+    return (
+        <footer className="otherinfo" id="multipage">
+            <div className="footerbox">
+                <div className="footerhead">Site by</div>
+                <div className="footertext">Robbie Boone</div>
+                <div className="footertext">wrboone12@gmail.com</div>
+            </div>
+            <div className="footerbox">
+                <div className="footerhead">Last modified</div>
+                <div className="footertext">8/01/2025</div>
+            </div>
+            <div className="footerbox">
+                <div className="footerhead">Intellectual Property</div>
+                <div className="footertext">All photos taken by me</div>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
---
+++ src/data/cats.js
@@ -0,0 +1,18 @@
+// src/data/cats.js
+export const catData = [
+  { id: 'sid', name: 'Sid', thumbnail: '/images/Sid/sidthumbnail.jpg' },
+  { id: 'ember', name: 'Ember', thumbnail: '/images/Ember/emberthumbnail.jpg' },
+  { id: 'biscuit', name: 'Biscuit', thumbnail: '/images/Biscuit/biscuitthumbnail.jpg' },
+  { id: 'emmett', name: 'Emmett', thumbnail: '/images/Emmett/emmetthumbnail.jpg' },
+  { id: 'lucy', name: 'Lucy', thumbnail: '/images/Lucy/lucythumbnail.jpg' },
+  { id: 'gunny', name: 'Gunny', thumbnail: '/images/Gunny/gunnythumbnail.jpg' },
+  { id: 'brother', name: 'Brother', thumbnail: '/images/brother/brotherthumbnail.jpg' },
+  { id: 'violin', name: 'Violin', thumbnail: '/images/Violin/violinthumbnail.jpg' },
+  { id: 'tulip', name: 'Tulip', thumbnail: '/images/Tulip/tulipthumbnail.jpg' },
+  { id: 'odysseus', name: 'Odysseus', thumbnail: '/images/Odysseus/odysseusthumbnail.jpg' },
+  { id: 'milo', name: 'Milo', thumbnail: '/images/Milo/milothumbnail.jpg' },
+  { id: 'guinness', name: 'Guinness', thumbnail: '/images/Guinness/guinnessthumbnail.jpg' },
+  { id: 'ernie', name: 'Ernie', thumbnail: '/images/Ernie/erniethumbnail.jpg' },
+  { id: 'beebo', name: 'Beebo', thumbnail: '/images/Beebo/beebothumbnail.jpg' },
+  { id: 'max', name: 'Max', thumbnail: '/images/Max/maxthumbnail.jpg' },
+];
