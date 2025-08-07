08e35e8774697e3ae05a780ef85ef72f7db59c98
Robbie Boone <wrboone@ncsu.edu>
2025-08-07 14:41:45 -0400

    Messing with paths again again

--- src/data/cats.js
+++ src/data/cats.js
@@ -1,10 +1,10 @@
 // src/data/cats.js
 export const catData = [
-  { id: 'sid', name: 'Sid', thumbnail: '/images/Sid/sidthumbnail.jpg' },
-  { id: 'ember', name: 'Ember', thumbnail: '/public/images/Ember/emberthumbnail.jpg' },
-  { id: 'biscuit', name: 'Biscuit', thumbnail: '/public/images/Biscuit/biscuitthumbnail.jpg' },
-  { id: 'emmett', name: 'Emmett', thumbnail: 'public/images/Emmett/emmetthumbnail.jpg' },
-  { id: 'lucy', name: 'Lucy', thumbnail: 'public/images/Lucy/lucythumbnail.jpg' },
+  { id: 'sid', name: 'Sid', thumbnail: 'CatCatalog/images/Sid/sidthumbnail.jpg' },
+  { id: 'ember', name: 'Ember', thumbnail: 'CatCatalog/public/images/Ember/emberthumbnail.jpg' },
+  { id: 'biscuit', name: 'Biscuit', thumbnail: './CatCatalog/images/Biscuit/biscuitthumbnail.jpg' },
+  { id: 'emmett', name: 'Emmett', thumbnail: './public/images/Emmett/emmetthumbnail.jpg' },
+  { id: 'lucy', name: 'Lucy', thumbnail: './images/Lucy/lucythumbnail.jpg' },
   { id: 'gunny', name: 'Gunny', thumbnail: 'public/images/Gunny/gunnythumbnail.jpg' },
   { id: 'brother', name: 'Brother', thumbnail: 'public/images/brother/brotherthumbnail.jpg' },
   { id: 'violin', name: 'Violin', thumbnail: 'public/images/Violin/violinthumbnail.jpg' },
